d525458f89a64f7578a15bb265afa8d4c3405554
jecavi20 <jecavi20@gmail.com>
2025-01-28 13:11:40 -0300

    Se implemento la funcion agregaramigo() en app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+//amigo
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value
+        if (amigo != '') {
+            amigos.push(amigo)
+            document.getElementById('amigo').value = ''
+            console.log(amigos)
+        }else{
+            alert('Por favor, inserte un nombre.')
+        }
+
+}
+
